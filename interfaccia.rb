ad28aad145a235dc0cb7f6c87717f22994cd75b8
dago19 <alevanni19@gmail.com>
2015-03-28 00:38:48 +0100

    Ho messo le barre coi bottoni. Metti i retangoli con lo stato ai lati.

--- interfaccia.rb
+++ interfaccia.rb
@@ -1,27 +1,34 @@
 #!/usr/bin/ruby
 require './quid.rb'
+require './barre_bottoni.rb'
 require 'tk'
 #un abbozzo dell'interfaccia grafica
 
 class Interfaccia
-  attr_accessor :finestra #in realta' non serve che siano attributi
- attr_accessor :f2
+ # attr_accessor :finestra #in realta' non serve che siano attributi
+  attr_accessor :f2
+  attr_accessor :barra_sopra
+  attr_accessor :barra_sotto
 def initialize
   @finestra=TkRoot.new {
-            height 200
+           height 200
             width  300 
             title "Quid"
   }
+
+  @barra_sopra=Barre_bottoni.new 'top', 'mangia', 'cura'
+   
   @f2 = TkFrame.new {
         relief 'groove'
         borderwidth 10
         height 200
         width 300
-        background "grey"
+        background "white"
         padx 20
-        pady 20
+        pady 60
         pack('side' => 'top')
   }
+  @barra_sotto=Barre_bottoni.new 'bottom', 'gioca', 'lava'
 end
 
 end
