868af4eaa3169612c5ded582f64a4d8726ed64ba
dago19 <alevanni19@gmail.com>
2015-03-28 23:43:43 +0100

    L'interfaccia e' uguale a prima, ma ho messo il canvas

--- interfaccia.rb
+++ interfaccia.rb
@@ -9,11 +9,10 @@ class Interfaccia
   attr_accessor :f2
   attr_accessor :barra_sopra
   attr_accessor :barra_sotto
+  attr_accessor :canvas #
 def initialize
-  @finestra=TkRoot.new {
-           height 200
-            width  300 
-            title "Quid"
+  @finestra=TkRoot.new{
+      title "Quid"
   }
 
   @barra_sopra=Barre_bottoni.new 'top', 'mangia', 'cura'
@@ -23,14 +22,18 @@ def initialize
         borderwidth 10
         height 200
         width 300
-        background "white"
-        padx 20
-        pady 60
         pack('side' => 'top')
   }
   @barra_sotto=Barre_bottoni.new 'bottom', 'gioca', 'lava'
+
+  @canvas=TkCanvas.new(@f2) {
+  background 'white'
+  pack('padx'=>1, 'pady'=>1, 'side'=>'top', 'expand'=>1)
+   }
+  @canvas.pack
 end
 
 end
 a=Interfaccia.new
+
 Tk.mainloop
