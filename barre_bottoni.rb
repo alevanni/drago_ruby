02cc937cf1d505e5abd78e26821d65ef0df5b75b
dago19 <alevanni19@gmail.com>
2015-03-29 18:57:46 +0200

    Ho rifatto le barre dei bottoni a modino.

--- barre_bottoni.rb
+++ barre_bottoni.rb
@@ -5,30 +5,50 @@ class Barre_bottoni
   attr_accessor :bottone1
   attr_accessor :bottone2
   attr_accessor :f
-
- def initialize posizione, comando1, comando2
+  attr_accessor :canvas1
+  attr_accessor :canvas2
+ def initialize dove, comando1, comando2
   @f = TkFrame.new {
         relief 'groove'
         borderwidth 10
-        height 50
-        width 300
+        height 30
+        width 50
         background "grey"
-        padx 20
-        pady 20
-        pack('side' => posizione)
+        padx 2
+        pady 2
+        pack('side'=>dove)
   }
+  @canvas1=TkCanvas.new(@f){
+    background 'grey'
+    height 40
+    width 30
+    grid('row'=>0,'column'=> 0) 
+   }
+  @canvas2=TkCanvas.new(@f){
+   background 'grey'
+   height 40
+    width 30
+   grid('row'=>0,'column'=> 3)
+   }
   @bottone1=TkButton.new(@f) {
    text comando1
-   pack("side" => "right",  "padx"=> "2", "pady"=> "2")
+   height 2
+   width 4
+   grid('row'=>0,'column'=> 1)
+   #inserisci i comandi!!
    }
   @bottone2=TkButton.new(@f) {
    text comando2
-   pack("side" => "left", "padx"=> "2", "pady"=> "2")
+   height 2
+   width 4
+   #inserisci i comandi!!
+   grid('row'=>0,'column'=> 2)
    }
+  
  end
 
 
 end
 
-prova=Barre_bottoni.new 'top', 'cibo', 'salute'
-Tk.mainloop
+#prova=Barre_bottoni.new 'top', 'cibo', 'salute'
+#Tk.mainloop
